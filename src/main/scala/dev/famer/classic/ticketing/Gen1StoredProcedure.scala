5048e109e50f5985690c1ef1b6a22b950ae27f09
My <my@famer.me>
2022-09-07 22:38:08 +0800

    [wip] gen1 ticketing

--- src/main/scala/dev/famer/classic/ticketing/Gen1StoredProcedure.scala
+++ src/main/scala/dev/famer/classic/ticketing/Gen1StoredProcedure.scala
@@ -5,18 +5,39 @@ import zio.ZIO
 
 import java.sql.SQLException
 import javax.sql.DataSource
+import zio.EnvironmentTag
+import zio.{Scope, ZIOAppArgs, ZLayer}
+import zio.{Scope, ZIOAppArgs}
+import io.getquill.jdbczio.Quill
+import zio.stream.ZStream
 
 case class Order(id: Int)
 
-object Gen1StoredProcedure {
+object Gen1StoredProcedure extends zio.ZIOAppDefault {
+  val datasourceLayer = Quill.DataSource.fromPrefix("testPostgresDB")
+  override def run: ZIO[Environment with ZIOAppArgs with Scope,Any,Any] = {
+
+    val io = ZStream.range(1, 1000000)
+      .mapZIOPar(128) { uid => DB.ordering(uid, 1) }
+      .runDrain
+
+    val flow = for {
+      res <- io.timed
+      _ <- ZIO.log(s"Done! time: ${res._1}")
+    } yield ()
+
+    flow.provideSomeLayer(datasourceLayer)
+  }
+
+}
+
+object DB {
   lazy val ctx = new PostgresZioJdbcContext(SnakeCase)
 
   import ctx._
   def ordering(userId: Int, productId: Int): ZIO[DataSource, SQLException, Int] = run {
     quote {
-      sql"SELECT public.ordering($userId, $productId)".as[Int]
+      sql"SELECT public.ordering(${lift(userId)}, ${lift(productId)})".as[Int]
     }
   }
-
 }
-
